df5444e6b1111c75620294dea5e1f0d7a8848794
zlliu246 <zlliu246@gmail.com>
2025-11-25 21:30:21 +0800

    added initial data for AAPL, BTC, VOO, GLD

--- scripts/polygon_raw_to_csv.py
+++ scripts/polygon_raw_to_csv.py
@@ -1,3 +1,8 @@
+"""
+Notes on polygon.io
+- max num rows is 50000, so we're only able to get ~3 months of data at a time
+- for BTC, markets are 24/7, so we need more files here
+"""
 import os
 import json
 from pathlib import Path
@@ -16,10 +21,26 @@ def parse_file(filepath: Path) -> pd.DataFrame:
         data.append([row["o"], row["h"], row["l"], row["c"], row["v"], row["t"]])
     df = pd.DataFrame(data, columns=["open", "high", "low", "close", "volume", "timestamp"])
     df['datetime'] = pd.to_datetime(df['timestamp'], unit='ms')
-    breakpoint()
+    return df
+
+def display_datetime_range(filepath: Path) -> None:
+    # show earliest datetime and latest datetime
+    df = parse_file(filepath)
+    print(filepath, df["datetime"].min(), df["datetime"].max())
 
 for symbol in os.listdir(RAW_POLYGON_PATH):
-    filepath = RAW_POLYGON_PATH / symbol
-    df = parse_file(filepath)
-    # df.to_csv(str(CLEANED_DATA_PATH / f"{symbol}.csv"), index=False)
-    # print(f"finished parsing {symbol}")
+
+    dfs = []
+
+    for filename in sorted(os.listdir(RAW_POLYGON_PATH / symbol)):
+        filepath = RAW_POLYGON_PATH / symbol / filename
+        if not str(filepath).endswith(".json"):
+            continue
+        df = parse_file(filepath)
+        dfs.append(df)
+    
+    df_agg = pd.concat(dfs).drop_duplicates()
+    df_agg.to_csv(str(CLEANED_DATA_PATH / f"{symbol}.csv"), index=False)
+    print(f"completed cleaning for {symbol}")
+
+        
