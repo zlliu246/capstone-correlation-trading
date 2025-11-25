699258cecac18e372d13141d92859cd0ff6566ef
zlliu246 <zlliu246@gmail.com>
2025-11-25 23:04:44 +0800

    add code to resample minute data to 15min/hourly/daily data

--- scripts/polygon_raw_to_cleaned.py
+++ scripts/polygon_raw_to_cleaned.py
@@ -1,16 +1,19 @@
-"""
-Notes on polygon.io
-- max num rows is 50000, so we're only able to get ~3 months of data at a time
-- for BTC, markets are 24/7, so we need more files here
-"""
 import os
 import json
 from pathlib import Path
+from typing import Sequence
 
 import pandas as pd
 
 RAW_POLYGON_PATH = Path("rawdata/polygon")
 CLEANED_DATA_PATH = Path("data/polygon")
+
+def mkdir_if_not_exists(paths: Sequence[Path]) -> None:
+    for path in paths:
+        try:
+            os.mkdir(path)
+        except:
+            pass
 
 def parse_file(filepath: Path) -> pd.DataFrame:
     with open(filepath, "r") as f:
@@ -28,6 +31,16 @@ def display_datetime_range(filepath: Path) -> None:
     df = parse_file(filepath)
     print(filepath, df["datetime"].min(), df["datetime"].max())
 
+def resample_15min_hourly_daily(df):
+    OPTIONS = {"open": "first", "high": "max", "low": "min", "close": "last", "volume": "sum"}
+    df = df.copy()
+    df.index = pd.to_datetime(df.datetime)
+    return (
+        df.resample("15T").agg(OPTIONS).reset_index(),
+        df.resample("1H").agg(OPTIONS).reset_index(),
+        df.resample("1D").agg(OPTIONS).reset_index(),
+    )
+
 for symbol in os.listdir(RAW_POLYGON_PATH):
 
     dfs = []
@@ -39,8 +52,16 @@ for symbol in os.listdir(RAW_POLYGON_PATH):
         df = parse_file(filepath)
         dfs.append(df)
     
-    df_agg = pd.concat(dfs).drop_duplicates()
-    df_agg.to_csv(str(CLEANED_DATA_PATH / f"{symbol}.csv"), index=False)
+    df_minute = pd.concat(dfs).drop_duplicates()
+    df_15, df_hourly, df_daily = resample_15min_hourly_daily(df_minute)
+
+    mkdir_if_not_exists([CLEANED_DATA_PATH / interval for interval in ["minute", "15min", "hourly", "daily"]])
+
+    df_minute.to_csv(str(CLEANED_DATA_PATH / f"minute/{symbol}.csv"), index=False)
+    df_15.to_csv(str(CLEANED_DATA_PATH / f"15min/{symbol}.csv"), index=False)
+    df_hourly.to_csv(str(CLEANED_DATA_PATH / f"hourly/{symbol}.csv"), index=False)
+    df_daily.to_csv(str(CLEANED_DATA_PATH / f"daily/{symbol}.csv"), index=False)
+
     print(f"completed cleaning for {symbol}")
 
-        
+
